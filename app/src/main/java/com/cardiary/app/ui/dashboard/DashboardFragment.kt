09b817cb5fa4215fb9a1302a1edfd0c4b5f2586e
Samuil Ovcharov <95378188+SmVO0@users.noreply.github.com>
2026-04-23 22:41:00 +0300

    Update DashboardFragment and fragment_dashboard.xml

--- app/src/main/java/com/cardiary/app/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/cardiary/app/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,38 @@
 package com.cardiary.app.ui.dashboard
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.cardiary.app.R
+import com.cardiary.app.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment(R.layout.fragment_dashboard)
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddCar.setOnClickListener {
+            findNavController().navigate(R.id.addCarFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
